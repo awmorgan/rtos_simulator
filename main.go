052d6db8d1856a59876ee2f252bfac747a423764
Bill Morgan <arthurwilliammorgan@gmail.com>
2023-03-21 20:55:47 -0500

    Make the application more interactive with a command loop

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -17,25 +19,42 @@ func main() {
 	// Create a new scheduler
 	scheduler := NewScheduler()
 
-	if len(os.Args) < 2 {
-		fmt.Println("Invalid command. Use 'add' to add a task or 'run' to start the scheduler.")
-		return
-	}
+	reader := bufio.NewReader(os.Stdin)
 
-	switch os.Args[1] {
-	case "add":
-		addTask.Parse(os.Args[2:])
-		if *taskPriority <= 0 || *taskDuration <= 0 {
-			fmt.Println("Invalid task priority or duration. Must be greater than 0.")
-			return
+	for {
+		fmt.Print("> ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading input:", err)
+			continue
 		}
-		taskID := scheduler.taskQueue.Len() + 1
-		task := NewTask(taskID, *taskPriority, *taskDuration)
-		scheduler.AddTask(task)
-		fmt.Printf("Added task %d with priority %d and duration %d\n", task.ID, task.Priority, task.Duration)
-	case "run":
-		scheduler.Run()
-	default:
-		fmt.Println("Invalid command. Use 'add' to add a task or 'run' to start the scheduler.")
+
+		input = strings.TrimSpace(input)
+		args := strings.Split(input, " ")
+
+		if len(args) == 0 {
+			fmt.Println("Invalid command. Use 'add' to add a task, 'run' to start the scheduler, or 'quit' to exit.")
+			continue
+		}
+
+		switch args[0] {
+		case "add":
+			addTask.Parse(args[1:])
+			if *taskPriority <= 0 || *taskDuration <= 0 {
+				fmt.Println("Invalid task priority or duration. Must be greater than 0.")
+				continue
+			}
+			taskID := scheduler.taskQueue.Len() + 1
+			task := NewTask(taskID, *taskPriority, *taskDuration)
+			scheduler.AddTask(task)
+			fmt.Printf("Added task %d with priority %d and duration %d\n", task.ID, task.Priority, task.Duration)
+		case "run":
+			scheduler.Run()
+		case "quit":
+			fmt.Println("Exiting RTOS Simulator")
+			return
+		default:
+			fmt.Println("Invalid command. Use 'add' to add a task, 'run' to start the scheduler, or 'quit' to exit.")
+		}
 	}
 }
